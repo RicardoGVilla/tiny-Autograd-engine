37028dc93941b0b7125c7a94ba8a15bb95c5d0e1
Ricardo Gutierrez <ricardorgvillanueva@gmail.com>
2024-12-15 17:07:48 -0500

    intro

--- micrograd_from_scratch.py
+++ micrograd_from_scratch.py
@@ -1,11 +1,27 @@
+
+## import libraries 
+
 import math 
 import numpy as np
 import matplotlib.pyplot as plt 
 
+## define function
 def f(x):
     return 3*x**2 - 4*x + 5
 
+## plot function
+xs = np.arange(-5, 5, 0.25)
+ys = f(xs)
 
-a = f(3.0)
 
-print(a)
+
+## what is the derivative of f(x) = 3x^2 - 4x + 5 at any point x 
+h = 0.001
+x = 3.0 
+(f(x + h) - f(x)) / h
+
+
+
+
+
+    
