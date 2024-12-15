67b3541339b485a57903433b89e29d233334b384
Ricardo Gutierrez <ricardorgvillanueva@gmail.com>
2024-12-15 17:16:22 -0500

    intro

--- micrograd_from_scratch.py
+++ micrograd_from_scratch.py
@@ -14,14 +14,31 @@ xs = np.arange(-5, 5, 0.25)
 ys = f(xs)
 
 
-
 ## what is the derivative of f(x) = 3x^2 - 4x + 5 at any point x 
 h = 0.001
 x = 3.0 
 (f(x + h) - f(x)) / h
 
 
+# extra input values 
+a = 2.0
+b = - 3.0 
+c = 10.0 
+d = a*b + c 
+print(d)
 
 
+# looking for the derivative of d with respect to a, b, and c 
+a = 2.0
+b = - 3.0 
+c = 10.0 
+d1 = a*b + c 
+print(d)
 
-    
+d1 = a*b + c 
+a += h 
+d2 = a*b + c
+
+print("d1", d1)
+print("d2", d2)
+print("slope", (d2 - d1) / h)
